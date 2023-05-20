2a9ff05a6d374e6660701e2d09bce942d1d7a04c
Anton Ödman <anton.odman@gmail.com>
2023-05-20 23:17:39 +0200

    chore: add basic metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
     title: "Ordkupan",
-    description: "Spela ordkupan!",
+    description: "Det svenska Spelling Bee-spelet",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className="h-full">
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
             <body className={`${inter.className} h-full`}>{children}</body>
         </html>
     );
