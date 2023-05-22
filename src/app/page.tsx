2e3d5c4a0b8fc742fc35d3905ec148d1a01bdfa9
Anton Ödman <anton.odman@gmail.com>
2023-05-22 09:35:43 +0200

    chore: add revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { Temporal } from "@js-temporal/polyfill";
 import { Playboard } from "../components/Playboard";
 import { getById } from "../utils/database";
 
+export const revalidate = 1000;
+
 export default async function Home() {
     const startDate = Temporal.ZonedDateTime.from({
         timeZone: "Europe/Stockholm",
