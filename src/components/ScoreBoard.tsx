82c52f466efc84fb319f01f3c26ff73c0e6bb89e
Anton Ödman <anton.odman@gmail.com>
2023-05-26 23:42:19 +0200

    chore: minor lint fixes

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -21,13 +21,13 @@ const labels = [
 ];
 
 const calculateStep = (score: number, maxScore: number, maxSteps: number) => {
-    if(score < 10) return 1;
+    if (score < 10) return 1;
 
-    if(score < 20) return 2;
+    if (score < 20) return 2;
 
     const step = Math.floor((score / maxScore) * maxSteps);
 
-    if(step < 2) return 2;
+    if (step < 2) return 2;
 
     return step;
 };
