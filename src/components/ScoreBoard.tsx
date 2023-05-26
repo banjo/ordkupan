8422c8fd0a96beda33e3a1444bf2461cbcb83716
Anton Ödman <anton.odman@gmail.com>
2023-05-26 22:10:28 +0200

    chore: Update ScoreBoard.tsx

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -21,19 +21,25 @@ const labels = [
 ];
 
 const calculateStep = (score: number, maxScore: number, maxSteps: number) => {
+    if(score < 10) return 1;
+
+    if(score < 20) return 2;
+
     const step = Math.floor((score / maxScore) * maxSteps);
-    console.log(step);
-    return step === 0 ? 1 : step;
+
+    if(step < 2) return 2;
+
+    return step;
 };
 
 export const ScoreBoard: FC<Props> = ({ score, maxScore, matchedWords }) => {
-    const step = calculateStep(score, maxScore, 10);
+    const step = calculateStep(score, maxScore, 9);
 
     return (
         <div className="flex flex-col justify-between gap-4">
             <div className="flex flex-col items-center justify-center">
                 <Stepper steps={9} active={step} display={score} />
-                <div>{labels[step]}</div>
+                <div>{labels[step - 1]}</div>
             </div>
             <WordField words={matchedWords} />
         </div>
