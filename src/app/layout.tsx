07b3edad06fcbdf400f30cfba6237d7f1d111dbc
Anton Ödman <anton.odman@gmail.com>
2023-06-01 20:20:18 +0200

    chore: add pwa assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import Script from "next/script";
+import { Assets } from "../components/Assets";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,12 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className="h-full">
-            <Head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
-            </Head>
+            <link rel="manifest" href="/site.webmanifest" />
+            <Assets />
 
             <Script
                 src="https://www.googletagmanager.com/gtag/js?id=G-PRBN0CL5K4"
