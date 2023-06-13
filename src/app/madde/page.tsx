75bdbdfc753e2fad6d5c2aa74206f437128e608e
Anton Ödman <anton.odman@gmail.com>
2023-06-13 22:11:23 +0200

    chore: fix madde page

--- src/app/madde/page.tsx
+++ src/app/madde/page.tsx
@@ -2,16 +2,16 @@ import { shuffle } from "@banjoanton/utils";
 import combos from "../../../words/data/combos.json" assert { type: "json" };
 import { Main } from "../../components/Main";
 import { Playboard } from "../../components/Playboard";
-import { Combo } from "../../types/types";
+import { BasicCombo } from "../../types/types";
 import { getCombos } from "../../utils/combo";
 
 export const revalidate = 0;
 
-const COMBOS = combos as Combo[];
+const COMBOS = combos as BasicCombo[];
 
-export default function Home() {
+export default async function Home() {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_, previousCombo, daysSinceStart] = getCombos();
+    const [_, previousCombo, daysSinceStart] = await getCombos();
     const combo = COMBOS[daysSinceStart + 1000];
     combo.otherLetters = shuffle(combo.otherLetters);
 
