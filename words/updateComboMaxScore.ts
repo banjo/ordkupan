026495fb66b0644e0f8d75425e6aa01eaf9fcdfb
Anton Ödman <anton.odman@gmail.com>
2023-07-12 11:10:46 +0200

    chore: add script for comboMaxScore fix

---
+++ words/updateComboMaxScore.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import fs from "node:fs";
+const prisma = new PrismaClient();
+
+const main = async () => {
+    const allCombos = await prisma.combo.findMany({
+        include: {
+            words: true,
+        },
+    });
+
+    const comboWithWrongMaxScore = allCombos.filter(combo => {
+        const maxScore = combo.words.reduce((acc, word) => acc + word.score, 0);
+
+        return maxScore !== combo.maxScore;
+    });
+
+    const output = comboWithWrongMaxScore.map(combo => {
+        const maxScore = combo.words.reduce((acc, word) => acc + word.score, 0);
+
+        return `UPDATE public."Combo" SET "maxScore"=${maxScore} WHERE id=${combo.id};`;
+    });
+
+    fs.writeFileSync("./updateComboMaxScore.sql", output.join("\n"));
+};
+
+main();
