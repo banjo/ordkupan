59a26603c9e7962bf898dbee546005a8206c208a
Anton Ödman <anton.odman@gmail.com>
2023-07-12 15:37:53 +0200

    chore: update rules

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -9,9 +9,19 @@ export const Rules: FC = () => {
                     <div className="mb-1 text-xl font-bold">Så här spelar du</div>
                     <div className="text-gray-600">
                         Med hjälp av de 7 bokstäverna på skärmen ska du bilda så många ord som
-                        möjligt. Mittenbokstaven måste användas i varje ord. Varje bokstav får
-                        användas hur många gånger som helst. Orden måste vara minst 4 bokstäver.
-                        Varje dag publiceras ett nytt spel.
+                        möjligt.
+                    </div>
+
+                    <div className="text-gray-600 mt-4">
+                        Mittenbokstaven måste användas minst en gång i varje ord. Varje bokstav får
+                        användas hur många gånger som helst. Orden måste vara minst 4 bokstäver
+                        långa. Varje dag publiceras ett nytt spel och gårddagens ord går att se i
+                        menyn.
+                    </div>
+
+                    <div className="text-gray-600 mt-4">
+                        Endast grundformen av ordet fungerar. Exempelvis fungerar &quot;ramsa&quot;
+                        som ord, men inte &quot;ramsor&quot; eller &quot;ramsan&quot;.
                     </div>
                 </div>
 
@@ -25,6 +35,17 @@ export const Rules: FC = () => {
                                 Det finns alltid minst ett ord som innehåller alla bokstäver, det
                                 ger 7 poäng extra.
                             </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="mb-1 mt-2 text-xl font-bold">Exempel</div>
+                    <div className="text-gray-600">
+                        <ul className="list-inside list-disc">
+                            <li>{"Saga -> 1 poäng"}</li>
+                            <li>{"Mossa -> 2 poäng"}</li>
+                            <li>{"Moussaka -> 5 poäng"}</li>
                         </ul>
                     </div>
                 </div>
