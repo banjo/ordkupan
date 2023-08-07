11b382bd52e8cab998300b5663e0320049fc3dd2
Anton Ödman <anton.odman@gmail.com>
2023-08-07 17:33:58 +0200

    chore: update local db to be more like prod

--- scripts/mock-prod-db.mjs
+++ scripts/mock-prod-db.mjs
@@ -11,10 +11,10 @@ await $`scp root@91.107.197.143:/root/db-backups/ordkupan_backup_${currentDate}.
 
 echo`Restoring database...`;
 await $`nr db:local:reset`;
-await $`PGPASSWORD=postgres psql -U postgres -h localhost -p 5432 -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = 'ordkupan';" -c "DROP DATABASE ordkupan;" -c "CREATE DATABASE ordkupan;"`;
+await $`PGPASSWORD=postgres psql -U ordkupan -h localhost -p 5432 -d postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = 'ordkupan';" -c "DROP DATABASE ordkupan;" -c "CREATE DATABASE ordkupan;"`;
 
 echo`Importing database...`;
-await $`PGPASSWORD=postgres psql -U postgres -d ordkupan -h localhost -p 5432 -f ./ordkupan_backup_${currentDate}.sql`;
+await $`PGPASSWORD=postgres psql -U ordkupan -d ordkupan -h localhost -p 5432 -f ./ordkupan_backup_${currentDate}.sql`;
 
 echo`Cleaning up...`;
 await $`rm ./ordkupan_backup_${currentDate}.sql`;
