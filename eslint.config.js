0fa6699d39c27917c213765d01181bbd0aad76ef
Anton Ödman <anton.odman@gmail.com>
2025-12-21 19:11:23 +0100

    fix: prettier stuff

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
 import eslintConfig from "@banjoanton/eslint-config";
+import prettierConfig from "@banjoanton/eslint-config-prettier";
 
-export default [...eslintConfig];
+export default [...eslintConfig, ...prettierConfig];
